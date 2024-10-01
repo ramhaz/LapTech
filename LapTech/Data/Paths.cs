e455daee67c84a18429381e50d49235d063333bc
Halil Kitir <hak001@edu.zealand.dk>
2024-10-01 16:00:27 +0200

    dsadsa

--- LapTech/Data/Paths.cs
+++ LapTech/Data/Paths.cs
@@ -2,8 +2,8 @@
 {
     public class Paths
     {
-        public string JsonGPUPath = "C:\\Users\\Halils Pc\\OneDrive - Danmarks Tekniske Universitet\\Dokumenter\\GitHub\\LapTech\\LapTech\\Data\\GPU.json";
-        public string JsonCPUPath = "C:\\Users\\Halils Pc\\OneDrive - Danmarks Tekniske Universitet\\Dokumenter\\GitHub\\LapTech\\LapTech\\Data\\CPU.json";
-        public string JsonLaptopPath = "C:\\Users\\Halils Pc\\OneDrive - Danmarks Tekniske Universitet\\Dokumenter\\GitHub\\LapTech\\LapTech\\Data\\Laptop.json";
+        public string JsonGPUPath = "C:\\Users\\halil\\OneDrive\\Dokumenter\\GitHub\\LapTech\\LapTech\\Data\\GPU.json";
+        public string JsonCPUPath = "C:\\Users\\halil\\OneDrive\\Dokumenter\\GitHub\\LapTech\\LapTech\\Data\\CPU.json";
+        public string JsonLaptopPath = "C:\\Users\\halil\\OneDrive\\Dokumenter\\GitHub\\LapTech\\LapTech\\Data\\Laptop.json";
     }
 }
--- LapTech/Models/Laptop.cs
+++ LapTech/Models/Laptop.cs
@@ -7,12 +7,15 @@
 
         public int Storage { get; set; }
 
-        public required string Manufacturer { get; set; }
+        public string Manufacturer { get; set; }
 
         public int Id { get; set; }
 
-        public GPU GPUId { get; set; }
-        public CPU CPUId { get; set; }
+        public int GPUId { get; set; }
+        public int CPUId { get; set; }
+
+        public GPU GPU { get; set; }
+        public CPU CPU { get; set; }
 
         public int Price { get; set; }
 
