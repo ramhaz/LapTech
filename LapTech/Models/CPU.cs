f61648ed528a5e92dd0812d39f2f33af0379055b
ramhaz <160605483+ramhaz@users.noreply.github.com>
2024-10-01 12:37:02 +0200

    hej

--- LapTech/Models/CPU.cs
+++ LapTech/Models/CPU.cs
@@ -8,5 +8,11 @@
         public string Cores { get; set; }
         public string Speed { get; set; }
 
+        public string type { get; set; }
+        public string tysde { get; set; }
+        public string tfspe { get; set; }
+        public string tepe { get; set; }
+
+
     }
 }
