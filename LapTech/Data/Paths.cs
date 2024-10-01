c974ac594f113102c8e732cee8fa44ab1866cb0b
StureDan <std001@edu.zealand.dk>
2024-10-01 10:14:23 +0200

    JJJ

--- LapTech/Data/Paths.cs
+++ LapTech/Data/Paths.cs
@@ -2,5 +2,8 @@
 {
     public class Paths
     {
+        public string JsonGPUPath = "C:\\Users\\sture\\OneDrive\\Skrivebord\\HubProjects\\LapTech\\LapTech\\Data\\CPU.json";
+        public string JsonCPUPath = "C:\\Users\\sture\\OneDrive\\Skrivebord\\HubProjects\\LapTech\\LapTech\\Data\\GPU.json";
+        public string JsonLaptopPath = "C:\\Users\\sture\\OneDrive\\Skrivebord\\HubProjects\\LapTech\\LapTech\\Data\\Laptop.json";
     }
 }
--- LapTech/Repositories/JsonLaptopRepository.cs
+++ LapTech/Repositories/JsonLaptopRepository.cs
@@ -1,6 +1,40 @@
-﻿namespace LapTech.Repositories
+﻿using LapTech.Models;
+using LapTech.Interfaces;
+using LapTech.Data;
+
+namespace LapTech.Repositories
 {
     public class JsonLaptopRepository
     {
+        private string JsonGPUPath = new Paths().JsonGPUPath;
+        private string JsonCPUPath = new Paths().JsonCPUPath;
+        private string JsonLaptopPath = new Paths().JsonLaptopPath;
+
+        private List<Laptop> LaptopList;
+
+        public void CreateLaptop(Laptop laptop)
+        {
+            throw new NotImplementedException;
+        }
+
+        public void DeleteLaptop(Laptop laptop)
+        {
+            throw new NotImplementedException;
+        }
+
+        public List <Laptop> GetAllLaptops()
+        {
+            return JsonFile
+        }
+
+        public Laptop GetLaptopById(int id)
+        {
+            throw new NotImplementedException;
+        }
+
+        public void UpdateLaptop(Laptop laptop)
+        {
+            throw new NotImplementedException;
+        }   
     }
 }
